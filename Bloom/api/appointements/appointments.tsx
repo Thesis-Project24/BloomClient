f0756b18e5d02907943766a78c3f811c94c04b2c
Maryem-Harmessi <hermassimyriam@gmail.com>
2024-01-09 13:12:26 +0100

    commit to pull

--- Bloom/api/appointements/appointments.tsx
+++ Bloom/api/appointements/appointments.tsx
@@ -1,4 +1,4 @@
-import { useMutation, QueryClientProvider } from 'react-query';
+import { useMutation, useQuery, QueryClientProvider } from 'react-query';
 import axios from 'axios';
 
 const addWindow = ()=>{
@@ -17,4 +17,14 @@ const addWindow = ()=>{
       return mutation
 }
 
-export {addWindow}
+const getSlots = (windowId:string)=> {
+  const query= useQuery(['slots', windowId], async () => {
+    const response = await fetch(`http://${process.env.EXPO_PUBLIC_ipadress}:3000/appointment/slots/${windowId}`)
+    if (!response.ok) {
+      throw new Error('Network response was not ok')
+    }
+    return response.json()
+  })
+}
+
+export {addWindow,getSlots}
